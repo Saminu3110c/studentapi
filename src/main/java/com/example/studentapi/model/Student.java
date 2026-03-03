bec65606326d4eaf96bf6aab861954206ffed30b
Saminu Isah <saminu08148556630@gmail.com>
2026-03-03 11:02:58 +0100

    Update:Lombok used for the student model

--- src/main/java/com/example/studentapi/model/Student.java
+++ src/main/java/com/example/studentapi/model/Student.java
@@ -1,53 +1,71 @@
 package com.example.studentapi.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 
     private Integer id;
     private String name;
     private String email;
     private int age;
-
-    // Required for Spring (JSON -> Object conversion)
-    public Student() {}
-
-    public Student(Integer id, String name, String email, int age) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-
-    // ===== GETTERS =====
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    // ===== SETTERS =====
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
+
+
+// package com.example.studentapi.model;
+
+// public class Student {
+
+//     private Integer id;
+//     private String name;
+//     private String email;
+//     private int age;
+
+//     // Required for Spring (JSON -> Object conversion)
+//     public Student() {}
+
+//     public Student(Integer id, String name, String email, int age) {
+//         this.id = id;
+//         this.name = name;
+//         this.email = email;
+//         this.age = age;
+//     }
+
+//     // ===== GETTERS =====
+//     public Integer getId() {
+//         return id;
+//     }
+
+//     public String getName() {
+//         return name;
+//     }
+
+//     public String getEmail() {
+//         return email;
+//     }
+
+//     public int getAge() {
+//         return age;
+//     }
+
+//     // ===== SETTERS =====
+//     public void setId(Integer id) {
+//         this.id = id;
+//     }
+
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
+
+//     public void setAge(int age) {
+//         this.age = age;
+//     }
+// }
